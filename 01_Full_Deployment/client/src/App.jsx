5cc5aba7e17e93dea7e7bc6ee1ed58f678bfd36e
Rachit Garg <rachitgarg1208@gmail.com>
2026-03-05 17:16:48 +0530

    Frontend Done

---
+++ 01_Full_Deployment/client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>RoaR DeviL</h1>
+    <p>Hosting from 0 to production</p>
+    </>
+  )
+}
+
+export default App
