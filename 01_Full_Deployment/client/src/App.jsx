3f4fa7f4fbc5fc1122e71aa2b4d8192e3d98d6b1
Rachit Garg <rachitgarg1208@gmail.com>
2026-03-05 18:10:44 +0530

    feat: Frontend Done

--- 01_Full_Deployment/client/src/App.jsx
+++ 01_Full_Deployment/client/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [message, setMessage] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.log(err))
+  }, [])
   return (
     <>
     <h1>RoaR DeviL</h1>
-    <p>Hosting from 0 to production</p>
+    <p>{message}</p>
     </>
   )
 }
