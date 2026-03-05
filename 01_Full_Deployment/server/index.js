7bfd8282961c9035726acbce427bfa8df46b5459
Rachit Garg <rachitgarg1208@gmail.com>
2026-03-05 21:55:52 +0530

    docker file for frontend and backend

--- 01_Full_Deployment/server/index.js
+++ 01_Full_Deployment/server/index.js
@@ -20,6 +20,6 @@ app.get("/api/message", (req, res) => {
 
 const PORT = 4000 
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
 })
